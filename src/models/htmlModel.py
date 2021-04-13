fd9c334edc3fd6fe42f66f5254f2ab8dd4e5d459
Ardavan <a.sassani@gmail.com>
2021-04-13 19:52:28 -0400

    page downloader

--- src/models/htmlModel.py
+++ src/models/htmlModel.py
@@ -1,3 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
 class HtmlModel():
     def __init__(self,level, url):
 
@@ -8,3 +12,12 @@ class HtmlModel():
         self.htmlContent = ""
         self.styles = ""
         self.links = set()
+        self.children = set()
+        self.__getcontent__()
+
+    def __getcontent__(self):
+        respons = requests.get(self.url)
+        soup = BeautifulSoup(respons.content, 'html.parser')
+        for link in soup.find_all('a', attrs={'href': re.compile("^http(s)?://")}):
+            self.links.add(link.get('href'))
+        print(self.url)
