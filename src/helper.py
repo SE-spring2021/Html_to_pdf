7bb2386c2f29a16f6b4c2a52a186d2011de00558
Ardavan <a.sassani@gmail.com>
2021-04-19 14:07:15 -0400

    debug print root object

--- src/helper.py
+++ src/helper.py
@@ -6,22 +6,23 @@ class Helper():
 
     @staticmethod
     def printPagesTitles(root:HtmlModel):
-        result="Donloaded Pages:\n"
+        result=""
         pages = set()
         pages.add(root)
         while pages:
             page = pages.pop()
-            result.join(page.title,"\n")
+            result += page.title+"\t("+ page.url +")" + "\n"
             for p in page.children:
                 pages.add(p)
         return result
-
+    
+    # TODO: it needs to cover all root domain
     @staticmethod
     def getDomain(url):    
         parts = re.split("\/", url)
         match = re.match("([\w\-]+\.)*([\w\-]+\.\w{2,6}$)", parts[2]) 
         if match != None:
-            if re.search("\.uk", parts[2]): 
+            if re.search("\.uk", parts[2]):
                 match = re.match("([\w\-]+\.)*([\w\-]+\.[\w\-]+\.\w{2,6}$)", parts[2])
             return match.group(2)
         else: return '' 
