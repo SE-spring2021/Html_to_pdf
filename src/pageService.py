a6602feace9af1ef9b1fb806822fbff2adc553b0
pavan_ajitb <pavan.babajiyavar@gmail.com>
2021-04-22 15:33:17 -0400

    pageService.py update
    pageService.py update

--- src/pageService.py
+++ src/pageService.py
@@ -3,43 +3,64 @@ import os
 
 class PageService():
 
-    def __init__(self,baseUrl):
+    def __init__(self,baseUrl,pdfDict):
         self.baseUrl=baseUrl
-        self.translateLinks()
+        self.pdfDict=pdfDict
+        self.translateBasePDF()
   
-    def translateLinks(self):
-        checkUrl=self.baseUrl.replace("/","")
-        checkUrl=checkUrl.replace(":","")
-        print(checkUrl)
+    def translateBasePDF(self):
+        try:
+             dataDir="documents\\"
+            pdfList=os.listdir(dataDir+self.pdfDict[self.baseUrl])
+            print(pdfList)
 
-        dataDir="..\\output\\"
-        pdfList=os.listdir(dataDir)
-        print(pdfList)
+            pdf = pdfrw.PdfReader(dataDir+self.pdfDict[self.baseUrl]+".pdf")
+            new_pdf = pdfrw.PdfWriter()
 
-        pdf = pdfrw.PdfReader(dataDir+checkUrl+".pdf")
-        new_pdf = pdfrw.PdfWriter()
+            for page in pdf.pages: 
+                for annot in page.Annots or []:
+                    old_url = annot.A.URI
+                    old_url=old_url.replace("(","")
+                    old_url=old_url.replace(")","")
+                    if(self.pdfDict[old_url]+".pdf" in pdfList):
+                        outUrl="("+self.pdfDict[old_url]+".pdf)"
+                        new_url = pdfrw.objects.pdfstring.PdfString("("+self.pdfDict[old_url]+")")
+                        annot.A.URI = new_url
 
-        for page in pdf.pages: 
-            for annot in page.Annots or []:
-                old_url = annot.A.URI
-                
-                checkOldUrl=old_url.replace("/","")
-                checkOldUrl=checkOldUrl.replace(":","")
-                checkOldUrl=checkOldUrl.replace("(","")
-                checkOldUrl=checkOldUrl.replace(")","")
-                checkOldUrl=checkOldUrl.replace("httpwww.","")
-                print(checkOldUrl)
-                if(checkOldUrl+".pdf" in pdfList):
-                    print("Inside")
-                    print(checkOldUrl+".pdf")
-                    
-                    outUrl="("+checkOldUrl+".pdf)"
-                    new_url = pdfrw.objects.pdfstring.PdfString("(gsu.pdf)")
-                    annot.A.URI = new_url
+                new_pdf.addpage(page)    
 
-            new_pdf.addpage(page)    
+            new_pdf.write(dataDir+self.pdfDict[self.baseUrl]+".pdf")
+            if(self.translatePDFs(dataDir,pdfList)):
+                return True
+            else:
+                return False
+        except Exception as ex:
+            print(ex)
+            pass
 
-        new_pdf.write(dataDir+checkUrl+".pdf")
-        
-if __name__=="__main__":
-    pageServiceHelper=PageService("http://tinman.cs.gsu.edu/~raj/")
+    def translatePDFs(self,dataDir,pdfList):
+        try:
+            for url,title in self.pdfDict.items():
+                if(title!=self.pdfDict[self.baseUrl] and title in pdfList):
+                    pdf = pdfrw.PdfReader(dataDir+title+".pdf")
+                    new_pdf = pdfrw.PdfWriter()
+
+                    for page in pdf.pages: 
+                        for annot in page.Annots or []:
+                            old_url = annot.A.URI
+                            old_url=old_url.replace("(","")
+                            old_url=old_url.replace(")","")
+                            if(self.pdfDict[old_url]+".pdf" in pdfList):
+                                outUrl="("+self.pdfDict[old_url]+".pdf)"
+                                new_url = pdfrw.objects.pdfstring.PdfString("("+self.pdfDict[old_url]+")")
+                                annot.A.URI = new_url
+
+                        new_pdf.addpage(page)    
+
+                    new_pdf.write(dataDir+title+".pdf")
+            return True
+        except Exception as ex:
+            print(ex)
+            pass        
+# if __name__=="__main__":
+#     pageServiceHelper=PageService("http://tinman.cs.gsu.edu/~raj/")
